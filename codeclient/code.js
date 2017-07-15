7aa9838ec637750ef9c68239a94b67666ebc35f8
angryfrenchguy <angryfrenchguy@gmail.com>
2017-07-15 17:46:00 -0400

    enlevé le titre "aplication"

--- codeclient/code.js
+++ codeclient/code.js
@@ -31,3 +31,41 @@ function getID(form, date) {
       return true;
   });
 }
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("No geo for you!");
+    }
+}
+
+function showPosition(position) {
+    $.ajax({
+        url: 'http://www.mapquestapi.com/geocoding/v1/reverse?',
+        dataType: 'jsonp',
+        crossDomain: true,
+        data: {
+            key: 'eDSGrYBrR7A6TK3vptsjxPpztoL1H6sq',
+            location: position.coords.latitude + "," + position.coords.longitude
+        },
+        success: function(data) {
+            document.getElementById('etatGO').value = data.results[0].locations[0].adminArea3;
+            document.getElementById('etatLOG').value = data.results[0].locations[0].adminArea3;
+            document.getElementById('etatSTOP').value = data.results[0].locations[0].adminArea3;
+        },
+        error: function (request, status, error) {
+            alert(error);
+            console.log(request.statusText);
+            
+            document.getElementById('etatGO').value = "erreur";
+            document.getElementById('etatLOG').value = "erreur";
+            document.getElementById('etatSTOP').value = "erreur";
+    }
+    
+    
+//    console.log("Latitude: " + position.coords.latitude + 
+//    "<br>Longitude: " + position.coords.longitude);
+});
+
+}
